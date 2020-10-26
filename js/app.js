7c3a25f5fbb8a211035390203762ef0063870d67
marie <mariecharton21@gmail.com>
2020-10-26 14:12:14 +0100

    en declaratif

--- js/app.js
+++ js/app.js
@@ -1,164 +1,142 @@
 /**
  * Todolist
  */
-const app = {
 
-  //Je stocke le compteur ici : 
-  count:0, // on part de zéro
-
-
-  init: function() {
-
-    //Target du DOM 
-    app.todo = document.getElementById("todo"); 
-    
-    //La génération du form
-    app.createFrom();
-    //La génération du counter
-    app.createCounter();
-    //La génération de la liste 
-    app.createList();
-
-    
+const tasks = [
+  {
+    label: 'farine',
+    done: false,
   },
+  {
+    label: 'sucre',
+    done: true,
+  },
+  {
+    label: 'lait',
+    done: false,
+  }
+];
 
-
-  addTask: function(evt){
+const app = {
+  count: 0, // on part de 0 pour le compteur
+  todo: null, // cible dans le DOM
+  init: function () {
+    // Target du DOM
+    app.todo = document.getElementById('todo');
+    // Ménage
+    app.todo.innerHTML = '';
+    // Génération du Form
+    app.createForm();
+    // Génération du Counter
+    app.createCounter();
+    // Génération de la List
+    app.createList();
+  },
+  addTask: function (evt) {
     evt.preventDefault();
-
-    //Récupération de la valeur du champ
-
-    //Solution 1
-    // const value = evt.target.elements.inputValue.value;
-
-    // Solution2
-    const formInput = document.querySelector("#todo-input");
+    console.log('submit');
+    // Ajouter une tache dans la liste des taches
+    const formInput = document.querySelector('#todo-input');
     const value = formInput.value;
 
-    //Ajout de la valeur du champ a la liste
-    app.generateTask({
-      label : value,
-      done : false,
+    // Ajout d'une tache aux données et non à l'interface
+    tasks.push({
+      label: value,
+      done: false,
     });
 
-    //On vide le champ
-    evt.target.elements.inputValue.value = "";
-
-    //Ajouter 1 au compteur 
-    app.count++;
-    app.updateCounter();
-  },
-  createFrom: function(){
-    console.log ("app :createForm");
-
-  //on crée l'élément un form
-  const form = document.createElement("form");
-  //on définit l'attribut id du form
-  form.id = 'todo-form'; 
-
-  form.addEventListener ("submit",app.addTask);
-
-  //Ajout d'un input text et l'ajouter au form
-  const input = document.createElement("input");
-  input.type = "text";
-  input.id = "todo-input";
-  input.autocomplete = "off";
-  input.name = "inputValue";
-  input.placeholder = "Ajouter une tâche";
-
-
-  //j'ajoute le form a l'input et le form au DOM 
-  form.appendChild(input);
-  app.todo.appendChild(form);
+    // Je recharge l'interface
+    app.init();
 
   },
-  //Mettre a jour le DOM avec la valeur du compteur 
-  updateCounter: function(){
-    const counter = document.getElementById ("todo-counter");
-    counter.textContent = `${app.count} tâches en cours`;
+  createForm: function () {
+    console.log('app : createForm');
+    // ajout d'un <form>
+    const form = document.createElement('form');
+    form.id = 'todo-form';
+
+    form.addEventListener('submit', app.addTask);
+
+    // ajout d'un input text et l'ajouter au form
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.id = 'todo-input';
+    input.autocomplete = 'off';
+    input.name = 'inputValue';
+    input.placeholder = 'Ajouter une tâche';
+
+    // ajout du form dans le dom
+    form.appendChild(input);
+    app.todo.appendChild(form);
   },
-  createCounter: function(){
-    console.log ("app :createCounter");
-
-    //Ajout d'une div 
-    const counter = document.createElement ("div");
-    counter.id = "todo-counter";
-    //Préparer le contenu 
-    counter.textContent = `${app.count} tâches en cours`;
-
-    //Ajout du compteur au DOM
-    app.todo.appendChild ( counter);
-
+  // MAJ du DOM avec la valeur du compteur
+  updateCounter: function () {
+    const counter = document.getElementById('todo-counter');
+    counter.textContent = `${app.count} tâche(s) en cours`;
   },
-  createList: function(){
-    console.log ("app :createList");
+  createCounter: function () {
+    console.log('app : createCounter');
+    // ajout d'un div
+    const counter = document.createElement('div');
+    counter.id = 'todo-counter';
 
+    // Préapration des taches non effectuées
+    const tasksUndone = tasks.filter((task) => !task.done);
+
+    // Préparer le contenu
+    counter.textContent = `${tasksUndone.length} tâche(s) en cours`;
+
+    // ajout du compteur au DOM
+    app.todo.appendChild(counter);
+  },
+  createList: function () {
+    console.log('app : createList');
     // Création de la liste
-    const list = document.createElement ("ul");
-    list.id = "task-list";
-
-    //Je stocke dans app pour y accèder partout
+    const list = document.createElement('ul');
+    list.id = 'tasks-list';
+    // je stocke dans tout app mon élément list (accessible de partout)
     app.list = list;
+    // J'affiche les taches de l'utilisateur
+    tasks.forEach(app.generateTask);
 
-
-  
-    // Ajout dans le DOM
-    app.todo.appendChild (list);
+    // ajout au DOM
+    app.todo.appendChild(list);
   },
-  generateTask : function (data){
-    //Ajouter une li 
-    const task = document.createElement("li");
-    task.className = "task";
+  // gère la création d'un li (tache)
+  generateTask: function (data) {
+    // ajout d'un li + enrichissements
+    const task = document.createElement('li');
+    task.className = 'task';
 
-    if(data.done){
-      task.classList.add ( "task-label--done");
+    if (data.done) {
+      task.classList.add('task-label--done');
     }
 
-    //Ajout de la checkbox
-    const checkbox = document.createElement ("input");
-    checkbox.type = "checkbox";
+    // ajout d'un checkbox
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
     checkbox.checked = data.done;
 
-    //ON écoute le changement sur les checkbox
-    checkbox.addEventListener ("change",function (){
-      //Changer la classe (toggle)
-      task.classList.toggle("task-label--done");
+    // Je vais écouter le changement sur la checkbox
+    checkbox.addEventListener('change', function () {
+      data.done = !data.done;
+      app.init();
+    });
 
-      //Baisser le compteur
-      if (checkbox.checked){
-        app.count--;
-      } else { app.count++;
-      }
-      // On met a jour le compteur 
-      app.updateCounter();
-    })
-
-    //Ajout du label span
-    const label = document.createElement("span");
+    // ajout d'un label => span
+    const label = document.createElement('span');
+    label.className = 'task-label';
     label.textContent = data.label;
-    label.className = "task-label";
 
-    //Ajout du checkbox et span a la li 
+    // ajouter checkbox et span au li
     task.appendChild(checkbox);
     task.appendChild(label);
 
-    //Ajout du li a la liste de tâches
+    // ajouter le li à la liste de taches
     app.list.appendChild(task);
   },
-
 };
 
 
 // Chargement du DOM
 document.addEventListener('DOMContentLoaded', app.init);
-    
-
-
-
-
-
-
-
-
-
-
